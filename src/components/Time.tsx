c497ee7d1b4094326bc895b4f8ac2ad3db0ba16d
Kseniia Savitskaia <savitskaya.kseniya.22@yandex.ru>
2023-05-05 21:45:26 +0300

    refactor: time

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { formatTime, getTime } from "../utils";
 
-export const StyledTime = styled("div")`
-  color: #fcdffd;
+export const StyledTime = styled("span")`
   font-weight: bold;
   font-size: 4rem;
+  flex-grow: 2;
+  text-align: center;
 `;
 
 export function Time() {
-  const [time] = useState(formatTime(getTime()));
-  /*
-  setInterval(() => {
-    const newTime = formatTime(getTime());
-    setTime(newTime);
-  }, 1000); */
+  const [time, setTime] = useState(formatTime(getTime()));
+
+  useEffect(() => {
+    setTimeout(() => {
+      const newTime = formatTime(getTime());
+      setTime(newTime);
+    }, 1000);
+  });
 
   return (
     <StyledTime>
-      {time.hours}:{time.minutes}:{time.seconds}
+      {time.hours} : {time.minutes} : {time.seconds}
     </StyledTime>
   );
 }
