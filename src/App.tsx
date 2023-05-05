abfed97713fa92d60dc630be184d2446ebd68c0f
Kseniia Savitskaia <savitskaya.kseniya.22@yandex.ru>
2023-05-05 16:19:17 +0300

    feat: add normalize and globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
 import { Time } from "./components/Time";
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  body {
+    background-color: black;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Time />
     </div>
   );
