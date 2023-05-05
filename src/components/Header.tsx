b2dba15d1a689972798d0fa60e88e8cb02eb9760
Kseniia Savitskaia <savitskaya.kseniya.22@yandex.ru>
2023-05-05 22:20:25 +0300

    feat: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { LogInCircle } from "@styled-icons/boxicons-regular";
 import { Time } from "./Time";
+import Button from "../styledComponents/Button";
 
 export const StyledHeader = styled("header")`
-  background: rgba(5, 2, 50, 0.5);
+  background: rgba(0, 0, 0, 0.5);
+  padding: 1rem;
+  display: flex;
+  align-items: center;
 `;
 
 export function Header() {
   return (
     <StyledHeader>
       <Time />
+      <Button>
+        <LogInCircle title="Login" size="48" />
+      </Button>
     </StyledHeader>
   );
 }
