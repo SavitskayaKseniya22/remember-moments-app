d76c728c6d4da204ca38479d8c920ac9deefd0ac
Kseniia Savitskaia <savitskaya.kseniya.22@yandex.ru>
2023-05-06 18:34:24 +0300

    feat: add time style

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-export const StyledTime = styled("span")`
-  font-weight: bold;
-  font-size: 4rem;
+export const StyledTimeBox = styled("div")`
   flex-grow: 2;
   text-align: center;
+`;
+
+export const StyledTime = styled("span")`
+  font-weight: bold;
+  font-size: 2rem;
+`;
+export const StyledDate = styled("span")`
+  font-weight: normal;
+  font-size: 1.5rem;
 `;
 
 export function Time() {
@@ -18,19 +25,24 @@ export function Time() {
   });
 
   return (
-    <StyledTime>
-      {time.toLocaleTimeString("en-Us", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      })}
+    <StyledTimeBox>
+      <StyledTime>
+        {time.toLocaleTimeString("en-Us", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        })}
+      </StyledTime>
+
       <br />
-      {time.toLocaleDateString("en-Us", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      })}
-    </StyledTime>
+      <StyledDate>
+        {time.toLocaleDateString("en-Us", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </StyledDate>
+    </StyledTimeBox>
   );
 }
