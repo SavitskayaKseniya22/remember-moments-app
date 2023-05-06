bff6f506325cabb32b55f3a1c704a9409cc19aec
Kseniia Savitskaia <savitskaya.kseniya.22@yandex.ru>
2023-05-06 15:03:13 +0300

    fix: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { GlobalStyle } from "./styledComponents/GlobalStyle";
@@ -9,14 +9,22 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route path="/" element="<div>main</div>" />
-          <Route path="/login" element="<div>login</div>" />
-          <Route path="/settings" element="<div>Settings</div>" />
-          <Route path="/*" element="<div>404</div>" />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Outlet />
+                <Footer />
+              </>
+            }
+          >
+            <Route path="/login" element="<div>login</div>" />
+            <Route path="/settings" element="<div>Settings</div>" />
+            <Route path="/*" element="<div>404</div>" />
+          </Route>
         </Routes>
-        <Footer />
       </BrowserRouter>
       <GlobalStyle />
     </>
