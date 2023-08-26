057b082f3b2f860b92c62ce4c9eedc8c528b3107
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2023-08-26 21:31:15 +0300

    refactor: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-import { Cog } from "@styled-icons/boxicons-regular";
+import { Cog, ArrowToTop } from "@styled-icons/boxicons-regular";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ReturnDownBack } from "@styled-icons/ionicons-outline";
 import { useTranslation } from "react-i18next";
-import { StyledRedButton } from "../styledComponents/StyledButton";
-import { flexboxLineStyle } from "../styledComponents/SharedStyles";
+import Button from "./Button";
 
 export const StyledFooter = styled("footer")`
-  background: rgba(0, 0, 0, 0.5);
-  ${flexboxLineStyle}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #666666;
+  padding: 1rem;
+
+  .footer__settings,
+  .footer__nav,
+  .footer__to-top {
+    position: fixed;
+    bottom: 0;
+  }
+
+  .footer__settings {
+    left: 0;
+  }
+
+  .footer__nav {
+    right: 0;
+  }
+
+  .footer__to-top {
+    top: 50%;
+    right: 0;
+    bottom: unset;
+  }
 `;
 
 export function Footer() {
@@ -19,25 +42,40 @@ export function Footer() {
 
   return (
     <StyledFooter>
-      <StyledRedButton
+      <Button
+        view="transparent"
+        className="footer__settings"
         handleClick={() => {
           navigate("settings");
         }}
         disabled={location.pathname === "/settings"}
       >
         <Cog title="Settings" size="36" />
-      </StyledRedButton>
-
+      </Button>
       <span>{t("madeBy")}</span>
-
-      <StyledRedButton
+      <Button
+        view="transparent"
+        className="footer__nav"
         handleClick={() => {
           navigate(-1);
         }}
         disabled={location.pathname === "/"}
       >
         <ReturnDownBack title="Back" size="36" />
-      </StyledRedButton>
+      </Button>
+      <Button
+        view="transparent"
+        className="footer__to-top"
+        handleClick={() => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        }}
+      >
+        <ArrowToTop title="ArrowToTop" size="36" />
+      </Button>
     </StyledFooter>
   );
 }
